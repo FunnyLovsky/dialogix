18d83f08e9a726881ec2de31d1d562f083ec900f
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-02-08 15:07:11 +0300

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("work");
+import './style';
+import style from './style/style.module.scss'
+
+
+function startApp() {
+    const root = document.getElementById('root');
+
+    const textArea = document.createElement('textarea');
+    textArea.className = style.text;
+    textArea.placeholder = 'Введите ваш текст...';
+
+    const btn = document.createElement('button');
+    btn.textContent = 'Создать';
+
+
+    root.append(textArea);
+    root.append(btn);
+
+    btn.onclick = () => {
+        const msg = textArea.value.length;
+
+        if(msg > 15000) {
+            console.log('Слишком большое сообщение!', msg);
+            return
+        }
+          
+
+        
+        
+    }
+    console.log("work");
+}
+
+startApp()
+
+
